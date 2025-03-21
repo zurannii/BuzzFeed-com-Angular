db7fd533044b1498a6c064fb8b476b650fad0df6
Roberta Nascimento <167623559+zurannii@users.noreply.github.com>
2025-03-21 18:25:07 -0300

    iniciando projeto

---
+++ buzzfeed-project/src/app/component/component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  imports: [],
+  templateUrl: './component.component.html',
+  styleUrl: './component.component.css'
+})
+export class ComponentComponent {
+
+}
